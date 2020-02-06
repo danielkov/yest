f475af99a0b0576b5cbb79c05439f8ab0198b458
Daniel Emod Kovacs <kovacsemod@gmail.com>
2020-02-06 23:00:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Package
+
+This is a minimal config for a (React +) TypeScript package to publish to NPM.
---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import bundleSize from 'rollup-plugin-bundle-size';
+import typescript from 'rollup-plugin-typescript2';
+import peerDeps from 'rollup-plugin-peer-deps-external';
+
+import pkg from './package.json';
+
+const plugins = [peerDeps(), typescript(), bundleSize()];
+
+const {
+  name,
+  source: input,
+  main,
+  browser,
+  module: mjs
+} = pkg;
+
+export default [{
+  input,
+  plugins,
+  output: [{
+      file: main,
+      format: 'cjs',
+    },
+    {
+      name,
+      file: browser,
+      format: 'umd',
+    },
+    {
+      format: 'esm',
+      file: mjs,
+    },
+  ],
+}, ];
